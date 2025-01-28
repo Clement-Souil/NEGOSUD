6d85cc1f546d46f7760f03596bdd6f617b77e75a
Clément SOUIL <souil.clement@outlook.fr>
2025-01-28 12:37:41 +0100

    Ajout du projet NegosudWeb

---
+++ NegosudWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NegosudWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
