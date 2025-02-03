da4e9042871e28547699b3178e2d74fdc60d8cf7
NathanEstay <147408550+NathanEstay@users.noreply.github.com>
2025-02-03 14:42:23 +0100

    save

---
+++ NEGOSUDClient/MVVM/ViewModels/MouvementStockViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEGOSUDClient.Services;
+using NegosudLibrary.DAO;
+
+namespace NEGOSUDClient.MVVM.ViewModels;
+
+public class MouvementStockViewModel
+{
+    public Article ArticleReference { get; set; }
+    public ObservableCollection<MouvementStock> ListMouvementStock { get; set; } = new();
+
+
+
+    public MouvementStockViewModel(Article article)
+    {
+        ArticleReference = article;
+        GetMouvement();
+    }
+
+    public void GetMouvement()
+    {
+        ListMouvementStock.Clear();
+
+        Task.Run(async () =>
+        {
+            return await HttpClientService.GetMouvementStock();
+        }).ContinueWith((t) =>
+        {
+            foreach (var MouvementStock in t.Result)
+            {
+                if (MouvementStock.ArticleId == ArticleReference.Id) 
+                {
+                    ListMouvementStock.Add(MouvementStock);
+                }
+            }
+        });
+    }
+}
